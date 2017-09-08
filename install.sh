7d890d3162c5f712c5472af0eb0c215c4684b748
John Scherff <jscherff@gmail.com>
2017-09-08 16:32:21 -0700

    Modified install script
    Modified install/uninstall scripts

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ useradd -M -r -d /opt/gocmdbd -c 'gocmdbd Service' gocmdbd
 
 mkdir -p /var/log/gocmdbd /opt/gocmdbd/{bin,etc}
 
+go build
+
 cp gocmdbd /opt/gocmdbd/bin
 cp config.json /opt/gocmdbd/etc
 cp sql/{database,users}.sql /opt/gocmdbd/etc
