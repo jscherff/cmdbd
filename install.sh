c13404d43957abfb9d0bd1566096dbe90df5c797
John Scherff <jscherff@gmail.com>
2017-09-08 16:26:14 -0700

    Modified install script

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ mkdir -p /var/log/gocmdbd /opt/gocmdbd/{bin,etc}
 
 cp gocmdbd /opt/gocmdbd/bin
 cp config.json /opt/gocmdbd/etc
+cp sql/{database,users}.sql /opt/gocmdbd/etc
 cp gocmdbd.service /etc/systemd/system
+
+cat /opt/gocmdbd/etc/*.sql | mysql -p
 
 chown -R gocmdbd:gocmdbd /var/log/gocmdbd /opt/gocmdbd
 
