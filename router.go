fe04004f8c76942542ed8815e75f9302c0e0d3ca
John Scherff <jscherff@gmail.com>
2017-09-10 17:51:26 -0700

    Changed URL paths

--- router.go
+++ router.go
@@ -38,10 +38,9 @@ func NewRouter() *mux.Router {
 		handler = handlers.RecoveryHandler(
 			handlers.PrintRecoveryStack(conf.Log.Options.RecoveryStack),
 			handlers.RecoveryLogger(conf.Log.Writer[Error]))(handler)
-/*
+
 		handler = handlers.ContentTypeHandler(handler,
 			conf.Server.AllowedContentTypes...)
-*/
 
 		router.
 			Methods(route.Method).
