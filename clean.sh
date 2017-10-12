476704c9ec451439aee94a16ee0da337de58eae6
John Scherff <jscherff@gmail.com>
2017-10-12 10:17:42 -0700

    Removed unnecessary files

--- clean.sh
+++ clean.sh
@@ -2,8 +2,8 @@
 
 git rm -f log
 git rm -f {i686,x86_64}/log
-git rm -f {i686,x86_64}/config.json
+git rm -f {i686,x86_64}/*.json
 
 rm -rf log
 rm -rf {i686,x86_64}/log
-rm -rf {i686,x86_64}/config.json
+rm -rf {i686,x86_64}/*.json
