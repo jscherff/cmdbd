66bcaf6c4be982eed420e90d40e8d1b43f9dd9a5
John Scherff <jscherff@gmail.com>
2018-01-17 08:29:54 -0800

    Added files previously removed to resolve merge conflicts

---
+++ bcrypt/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	`fmt`
+	`os`
+	`path/filepath`
+	`golang.org/x/crypto/bcrypt`
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: %s <word> [<word> ...]\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
+	}
+
+	for _, word := range os.Args[1:] {
+
+		if hash, err := bcrypt.GenerateFromPassword([]byte(word), bcrypt.DefaultCost); err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("%s %s\n", word, string(hash))
+		}
+	}
+}
